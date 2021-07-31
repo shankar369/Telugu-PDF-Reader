b94c2cd9ee0bb114d00ef6cdc09290281c46b46d
sairam150496 <sairam150496@gmail.com>
2021-07-31 14:04:01 +0530

    refactored and added support to save image and extracted text

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from utils import get_lib_dir, delimited_string
+from utils import get_lib_dir, delimited_string, find_or_create
 from dotenv import load_dotenv, dotenv_values
 from tesseract_comp import extract_info
 from pdf2imageconverter import convert_pdf_to_image
 import pytesseract
 from gtts import gTTS
-import playsound
 import os
+from PIL import Image
 
 
 
@@ -21,22 +21,25 @@ tesser_path = dotenv_values()['TESSER_PATH_']+f"{delimited_string}tesseract.exe"
 
 pytesseract.pytesseract.tesseract_cmd = f'''{os.getcwd()}{tesser_path}'''
 
-dir = "audios" + delimited_string
+audio_dir = "audios" + delimited_string
+image_dir = "images"+ delimited_string
+extracted_text = "text"+ delimited_string
 
 for file in os.listdir(pdf_path):
 
     if file.split('.')[-1] == 'pdf':
-        op_dir = dir+f'{file.split(".")[0]}{delimited_string}'
-        os.mkdir(op_dir)
+        f_dir = f'{file.split(".")[0]}{delimited_string}'
+        find_or_create([audio_dir+f_dir, image_dir+f_dir, extracted_text+f_dir])
         images = convert_pdf_to_image(pdf_path+f'{delimited_string}{file}', get_lib_dir(os.getcwd(),
                     delimited_string, dotenv_values()['POPPLER_BIN'], 0))
 
         cnt = 0
         for image in images:
             cnt += 1
+            image.save(image_dir+f_dir+f'{cnt}.jpg', "JPEG")
             print("extracting page: ", image)
-
+            with open(extracted_text+f_dir+f'{cnt}.txt', 'w', encoding="utf-8") as txt_file:
+                txt_file.write(extract_info(image))
             myobj = gTTS(text=extract_info(image), lang="te", slow=False)
-            myobj.save(op_dir+f'{cnt}.mp3')
-            break
+            myobj.save(audio_dir+f_dir+f'{cnt}.mp3')
 
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,3 @@
 from .getDelimiter import delimited_string
 from .getLibraryDir import get_lib_dir
+from .findOrCreate import find_or_create
