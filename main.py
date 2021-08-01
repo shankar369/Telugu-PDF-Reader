1b4ccb53fd427c9d96b2ce76ac0727fcc72f2321
shankar p <shankar369>
2021-08-01 12:19:04 +0530

    adding ui to upload files

--- main.py
+++ main.py
@@ -1,42 +1,68 @@
-from utils import get_lib_dir, delimited_string
+# from utils import get_lib_dir, delimited_string
 from dotenv import load_dotenv, dotenv_values
-from tesseract_comp import extract_info
-from pdf2imageconverter import convert_pdf_to_image
-import pytesseract
-from gtts import gTTS
-import playsound
+# from tesseract_comp import extract_info
+# from pdf2imageconverter import convert_pdf_to_image
+# import pytesseract
+# from gtts import gTTS
+# import playsound
 import os
 
+from fastapi import FastAPI, Request, File, UploadFile, BackgroundTasks, Form
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from scheduler import add_task
 
+import aiofiles
 
 load_dotenv()
 
-pdf_path = f'{os.getcwd()}{dotenv_values()["PDF_PATH"]}'
 
-tesser_data = os.getcwd()+dotenv_values()['TESSER_TES_DATS']
+app = FastAPI()
+app.queue = []
+app.mount(
+    "/static",
+    StaticFiles(directory= dotenv_values()["STATICS"]),
+    name="static",
+)
 
-os.environ['TESSDATA_PREFIX'] = tesser_data
+templates = Jinja2Templates(directory="Views")
 
-tesser_path = dotenv_values()['TESSER_PATH_']+f"{delimited_string}tesseract.exe"
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
-pytesseract.pytesseract.tesseract_cmd = f'''{os.getcwd()}{tesser_path}'''
 
-dir = "audios" + delimited_string
+@app.post("/pdf")
+async def handle_post_pdf(background_tasks: BackgroundTasks,pdf_file: UploadFile = File(...), ):
+    if(not pdf_file.content_type == "application/pdf"):
+        response.status_code = 404
+        return {
+            'error': 'Only PDF files are allowed'
+        }
+    try:
+        async with aiofiles.open(dotenv_values()["PDF_UPLOAD_PATH"]+pdf_file.filename, 'wb') as out_file:
+            while content := await pdf_file.read(1024):  # async read chunk
+                await out_file.write(content)  # async write chunk
+        
+        background_tasks.add_task(add_task, file_name=pdf_file.filename)
+        return {
+            "status": "uploaded",
+            "file_name": pdf_file.filename,
+            "fileb_content_type": pdf_file.content_type,
+        }
+    except Exception as ex:
 
-for file in os.listdir(pdf_path):
+        return {
+            "status": "error",
+            "message" : str(ex)
+        }
 
-    if file.split('.')[-1] == 'pdf':
-        op_dir = dir+f'{file.split(".")[0]}{delimited_string}'
-        os.mkdir(op_dir)
-        images = convert_pdf_to_image(pdf_path+f'{delimited_string}{file}', get_lib_dir(os.getcwd(),
-                    delimited_string, dotenv_values()['POPPLER_BIN'], 0))
+@app.get("/audios")
+def get_audio_folders(request: Request):
+    audio_folders = os.listdir(dotenv_values()["AUDIOS_PATH"])
+    return templates.TemplateResponse("audios.html",{"request":request,"folders":audio_folders})
 
-        cnt = 0
-        for image in images:
-            cnt += 1
-            print("extracting page: ", image)
-
-            myobj = gTTS(text=extract_info(image), lang="te", slow=False)
-            myobj.save(op_dir+f'{cnt}.mp3')
-            break
-
+@app.get("/audios/{folder_name}")
+def get_audio_folders(request: Request,folder_name):
+    audio_files = os.listdir(dotenv_values()["AUDIOS_PATH"]+folder_name)
+    return templates.TemplateResponse("audio.html",{"request":request,"audios":audio_files,"folder":folder_name})
---
+++ scheduler.py
@@ -0,0 +1,49 @@
+from utils import get_lib_dir, delimited_string
+from dotenv import load_dotenv, dotenv_values
+from tesseract_comp import extract_info
+from pdf2imageconverter import convert_pdf_to_image
+import pytesseract
+from gtts import gTTS
+import playsound
+import os
+
+
+load_dotenv()
+
+def process_pdf(file):
+    pdf_path = f'{os.getcwd()}{dotenv_values()["PDF_PATH"]}'
+
+    tesser_data = os.getcwd()+dotenv_values()['TESSER_TES_DATS']
+
+    os.environ['TESSDATA_PREFIX'] = tesser_data
+
+    tesser_path = dotenv_values()['TESSER_PATH_']+f"{delimited_string}tesseract.exe"
+
+    pytesseract.pytesseract.tesseract_cmd = f'''{os.getcwd()}{tesser_path}'''
+
+    dir = "statics/audios" + delimited_string
+
+    # for file in os.listdir(pdf_path):
+
+    #     if file.split('.')[-1] == 'pdf':
+    op_dir = dir+f'{file.split(".")[0]}{delimited_string}'
+    os.mkdir(op_dir)
+    images = convert_pdf_to_image(pdf_path+f'{delimited_string}{file}', get_lib_dir(os.getcwd(),
+                delimited_string, dotenv_values()['POPPLER_BIN'], 0))
+
+    cnt = 0
+    print(len(images),"----images count")
+    for image in images:
+        try:
+            cnt += 1
+            print("extracting page: ", image)
+
+            myobj = gTTS(text=extract_info(image), lang="te", slow=False)
+            myobj.save(op_dir+f'{cnt}.mp3')
+        except Exception as ex:
+            print(str(ex))
+
+
+def add_task(file_name):
+    print(file_name, " added for processing .......")
+    process_pdf(file_name)
